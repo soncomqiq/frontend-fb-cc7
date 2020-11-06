39324aafe51fc4164bb65454cbd87d6bab04a9ae
codecamp.official.thailand <codecamp.official.thailand@gmail.com>
2020-11-06 15:05:06 +0700

    add axios setup

--- src/containers/pages/Feed/Feed.js
+++ src/containers/pages/Feed/Feed.js
@@ -1,98 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PostList from '../../../components/PostList/PostList';
-
-const mockPostList = [
-  {
-    "id": 1,
-    "caption": "Google",
-    "picture_url": "https://i.pinimg.com/originals/b1/94/0e/b1940eb27d12eadbcdaa86dca0f1037d.jpg",
-    "createdAt": "2020-11-04T04:54:05.000Z",
-    "updatedAt": "2020-11-04T04:54:05.000Z",
-    "Comments": [
-      {
-        "id": 1,
-        "comment": "Hello",
-        "User": {
-          "id": 1,
-          "name": "Sonter InwZa",
-          "profile_url": "https://vignette.wikia.nocookie.net/kzorori/images/c/c2/Prologuebkg.png/revision/latest?cb=20110325181412"
-        }
-      }
-    ],
-    "User": {
-      "id": 1,
-      "name": "Sonter InwZa",
-      "profile_url": "https://vignette.wikia.nocookie.net/kzorori/images/c/c2/Prologuebkg.png/revision/latest?cb=20110325181412"
-    }
-  },
-  {
-    "id": 2,
-    "caption": "Hooley",
-    "picture_url": "",
-    "createdAt": "2020-11-04T04:54:22.000Z",
-    "updatedAt": "2020-11-04T04:57:34.000Z",
-    "Comments": [],
-    "User": {
-      "id": 1,
-      "name": "Sonter InwZa",
-      "profile_url": "https://vignette.wikia.nocookie.net/kzorori/images/c/c2/Prologuebkg.png/revision/latest?cb=20110325181412"
-    }
-  },
-  {
-    "id": 3,
-    "caption": "Yahoo",
-    "picture_url": "",
-    "createdAt": "2020-11-04T04:55:21.000Z",
-    "updatedAt": "2020-11-04T04:55:21.000Z",
-    "Comments": [
-      {
-        "id": 2,
-        "comment": "Hello",
-        "User": {
-          "id": 1,
-          "name": "Sonter InwZa",
-          "profile_url": "https://vignette.wikia.nocookie.net/kzorori/images/c/c2/Prologuebkg.png/revision/latest?cb=20110325181412"
-        }
-      }
-    ],
-    "User": {
-      "id": 2,
-      "name": "Pup Potato",
-      "profile_url": "https://dolamon19.files.wordpress.com/2013/01/pup-potato.jpg"
-    }
-  },
-  {
-    "id": 4,
-    "caption": "Yahoo",
-    "picture_url": "",
-    "createdAt": "2020-11-04T04:55:23.000Z",
-    "updatedAt": "2020-11-04T04:55:23.000Z",
-    "Comments": [],
-    "User": {
-      "id": 2,
-      "name": "Pup Potato",
-      "profile_url": "https://dolamon19.files.wordpress.com/2013/01/pup-potato.jpg"
-    }
-  },
-  {
-    "id": 5,
-    "caption": "Yahoo",
-    "picture_url": "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg",
-    "createdAt": "2020-11-04T04:55:24.000Z",
-    "updatedAt": "2020-11-04T04:55:24.000Z",
-    "Comments": [],
-    "User": {
-      "id": 2,
-      "name": "Pup Potato", 
-      "profile_url": "https://dolamon19.files.wordpress.com/2013/01/pup-potato.jpg"
-    }
-  }
-];
+import axios from '../../../config/axios';
 
 function Feed() {
+  const [postList, setPostList] = useState([]);
+
+  useEffect(() => {
+    axios.get("/posts/feed")
+      .then(res => {
+        setPostList(res.data);
+      });
+  }, []);
+
   return (
     <div>
-      <PostList postList={mockPostList} />
+      <PostList postList={postList} />
     </div>
   );
 }
